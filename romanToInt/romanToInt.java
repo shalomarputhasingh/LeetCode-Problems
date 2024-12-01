470e7213b262fc60a69b3a555ce21d1daec271c3
= <=>
2024-12-01 14:13:32 +0530

    Updated RomanToInt

--- romanToInt/romanToInt.java
+++ romanToInt/romanToInt.java
@@ -1,45 +1,32 @@
-import java.lang.classfile.components.ClassPrinter.LeafNode;
-import java.util.Scanner;
-class romanToInt{
-
+class Solution {
     static int returnNum(Character roman){
-        int I = 1;
-        int V = 5;
-        int X = 10;
-        int L = 50;
-        int C = 100;
-        int D = 500;
-        int M = 1000;
         if (roman == 'I'){
-            return I;
+            return 1;
         }
         else if (roman == 'V'){
-            return V;
+            return 5;
         }
         else if(roman == 'X'){
-            return X;
+            return 10;
         }
         else if (roman  == 'L'){
-            return L;
+            return 50;
         }
         else if(roman == 'C'){
-            return C;
+            return 100;
         }
         else if(roman == 'D'){
-            return D;
+            return 500;
         }
         else if(roman == 'M'){
-            return M;
+            return 1000;
         }
         else{
             return 0;
         }
     }
-    public static void main(String[] args) {
-        System.out.print("Enter the Roman: ");
-        Scanner scan = new Scanner(System.in);
-        String romans = scan.nextLine();
-        scan.close();
+    public static int romanToInt(String s) {
+        String romans = s;
         int length = romans.length();
         int num = 0;
         for (int i=0;i<length;i++){
@@ -59,7 +46,6 @@ class romanToInt{
                 num+=a;
             }
         }
-        System.out.println(num);
+        return num;
     }
-    
 }
